6fdfc3c64952bd1d1e1cbc74888976f8c50fcc7b
luieitalian <berkepite@hotmail.com>
2025-01-27 23:14:41 +0300

    Initial Commit

---
+++ src/test/java/com/berkepite/MainApplication32Bit/MainApplication32BitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkepite.MainApplication32Bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplication32BitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

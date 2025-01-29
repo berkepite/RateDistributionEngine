265cfc2af597fd86f9b9e1f2f7075fc5a35644ba
luieitalian <berkepite@hotmail.com>
2025-01-29 21:58:12 +0300

    chore: Add tests for SubscriberConfigLoader.

---
+++ src/test/java/com/berkepite/MainApplication32Bit/SubscriberConfigLoaderTest.java
@@ -0,0 +1,49 @@
+package com.berkepite.MainApplication32Bit;
+
+import com.berkepite.MainApplication32Bit.subscribers.SubscriberConfigLoader;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class SubscriberConfigLoaderTest {
+
+    @Autowired
+    SubscriberConfigLoader subscriberConfigLoader;
+
+    @Test
+    public void testLoadSubscriberConfig_validFile() {
+        Properties properties;
+        try {
+            properties = subscriberConfigLoader.readFromFile("subscriber");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        assertNotNull(properties, "Properties should not be null");
+        assertEquals("subscriber", properties.getProperty("subscriber.name"));
+    }
+
+    @Test
+    public void testLoadSubscriberConfig_fileNotFound() {
+        Exception exception = null;
+        try {
+            Properties properties = subscriberConfigLoader.readFromFile("subscribser");
+
+        } catch (Exception e) {
+            exception = e;
+        }
+
+        // Then an exception should be thrown
+        assertNotNull(exception, "Exception should be thrown when file is not found");
+    }
+}

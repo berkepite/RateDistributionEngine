6339dbc92cd9c77ed465c0728206593e743ff2fc
luieitalian <berkepite@hotmail.com>
2025-02-03 22:53:26 +0300

    chore(rate): Add rate fields.

--- src/main/java/com/berkepite/MainApplication32Bit/rates/IRate.java
+++ src/main/java/com/berkepite/MainApplication32Bit/rates/IRate.java
@@ -1,5 +1,21 @@
 package com.berkepite.MainApplication32Bit.rates;
 
-public interface IRate {
+import java.time.Instant;
 
+public interface IRate {
+    RateEnum getType();
+
+    void setType(RateEnum type);
+
+    Double getBid();
+
+    Double getAsk();
+
+    Instant getTimeStamp();
+
+    void setBid(Double bid);
+
+    void setAsk(Double ask);
+
+    void setTimeStamp(Instant timeStamp);
 }
--- src/main/java/com/berkepite/MainApplication32Bit/rates/Rate.java
+++ src/main/java/com/berkepite/MainApplication32Bit/rates/Rate.java
@@ -1,5 +1,50 @@
 package com.berkepite.MainApplication32Bit.rates;
 
+import java.time.Instant;
+
 public class Rate implements IRate {
     private RateEnum rateType;
+    private Double bid;
+    private Double ask;
+    private Instant timeStamp;
+
+    @Override
+    public RateEnum getType() {
+        return rateType;
+    }
+
+    @Override
+    public void setType(RateEnum type) {
+        rateType = type;
+    }
+
+    @Override
+    public Double getBid() {
+        return bid;
+    }
+
+    @Override
+    public Double getAsk() {
+        return ask;
+    }
+
+    @Override
+    public Instant getTimeStamp() {
+        return timeStamp;
+    }
+
+    @Override
+    public void setBid(Double bid) {
+        this.bid = bid;
+    }
+
+    @Override
+    public void setAsk(Double ask) {
+        this.ask = ask;
+    }
+
+    @Override
+    public void setTimeStamp(Instant timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
